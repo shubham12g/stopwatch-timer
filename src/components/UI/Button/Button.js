8927e7975a17829ab33b7567ec6c539d7d0e093a
Shubham Gupta <shubhamgupta1211@gmail.com>
2020-06-20 12:31:44 +0530

    updated button ui

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import classes from "./Button.module.css";
 
-const button = (props) => (
-  <button
-    className={classes.Button}
-    onClick={props.clicked}
-    disabled={props.disabled}
-  >
-    {props.name}
-  </button>
-);
+const button = (props) => {
+  const btnClass = [classes[props.color], classes.Button];
+
+  return (
+    <button
+      className={btnClass.join(" ")}
+      onClick={props.clicked}
+      disabled={props.disabled}
+    >
+      {props.name}
+    </button>
+  );
+};
 
 export default button;
